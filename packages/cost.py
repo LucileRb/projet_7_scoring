112025778e3c04d83947eb05c1d3e6cdbf7cb11a
LucileRb <124692104+Lucile-ads-up@users.noreply.github.com>
2024-04-25 09:45:25 +0200

    ajout modeles a tester

--- packages/cost.py
+++ packages/cost.py
@@ -11,6 +11,27 @@ from sklearn.metrics import confusion_matrix
 
 ########## Fonction de coût ##########
 
+# 1ère fonction coût avec modification du f1score -> intéressant mais résultats un peu tordus à interpreter
+#def custom_metric(y_true, y_pred):
+#    '''function pour pénaliser faux positifs en particulier'''
+#    tp, tn, fp, fn = confusion_matrix(y_true, y_pred).ravel()
+#    tp_weight = 1
+#    tn_weight = 1
+#    fn_weight = -1
+#    fp_weight = -10
+#    tp = tp * tp_weight
+#    tn = tn * tn_weight
+#    fn = fn * fn_weight
+#    fp = fp * fp_weight
+#    recall = tp / (tp + fn)
+#    precision = tp / (tp + fp)
+#    f1score = 2 * (precision * recall) / (precision + recall)
+#    return f1score
+
+# construire une fonction cout sans passer par le f1score
+# faire la somme des tp tn fn fp
+# quand la somme est positive : on fait de l'argent
+# quand elle est négative : on en perd
 
 def custom_metric(y_true, y_pred):
     '''function pour pénaliser faux positifs en particulier'''
@@ -27,13 +48,8 @@ def custom_metric(y_true, y_pred):
     fn = fn * fn_weight
     fp = fp * fp_weight
 
-    recall = tp / (tp + fn)
-    precision = tp / (tp + fp)
-    f1score = 2 * (precision * recall) / (precision + recall)
+    score = tp + tn + fn + fp
 
-    return f1score
+    return score
 
-# construire une fonction cout sans passer par le f1score
-# faire la somme des tp tn fn fp
-# quand la somme est positive : on fait de l'argent
-# quand elle est négative : on en perd
+############################### END ###############################z

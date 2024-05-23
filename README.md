56e7614a34838b4d817f65c68fe4e597ae7751a3
LucileRb <124692104+Lucile-ads-up@users.noreply.github.com>
2024-05-23 13:15:55 +0200

    update

--- README.md
+++ README.md
@@ -1,15 +1,32 @@
-# Implement a scoring model
+### Projet de Scoring Crédit
+Ce projet vise à développer un modèle de scoring de crédit pour prédire la probabilité de remboursement d'un prêt par un client. Le modèle est destiné à être utilisé par une institution financière pour évaluer les demandes de crédit et prendre des décisions éclairées sur l'octroi de prêts.
 
-# Descriptif du projet
-1) Mettre en oeuvre un outil de "scoring crédit" pour calculer la probabilié qu'un client rembourse son crédit
-2) classifier la demande en crédit accordé/refusé
+Lien vers les données : https://www.kaggle.com/c/home-credit-default-risk/data
 
--> algorithme de classification en s'appuyant sur des données variées (données comportementales, données provenant d'autres institutions financières, etc...)
--> réaliser un dashboard intéractif (Dash ou Bokeh ou Streamlit)
+# Structure du Projet
+Le projet est organisé de la manière suivante :
+- data/ : Ce répertoire contient les données utilisées pour l'entraînement et le test du modèle.
+- packages/ : Contient les scripts et les modules Python utilisés pour le prétraitement des données, l'entraînement du modèle, l'évaluation et l'interprétation des résultats.
+- mlruns/ : Répertoire utilisé par MLflow pour enregistrer les résultats de l'entraînement des modèles et les métriques associées.
+- mlartifacts/ : Contient les artefacts produits lors de l'entraînement des modèles, tels que les modèles entraînés et les visualisations.
+- results/ : Répertoire contenant les rapports et les résultats de l'évaluation du modèle, tels que les rapports sur le data drift et le target drift.
+- scoring_app/ : Une application web simple pour démontrer l'utilisation du modèle de scoring de crédit.
+- cours_et_exemples/ : Contient des ressources pédagogiques et des exemples utilisés pour développer le modèle.
+- images_presentation/ : Images utilisées pour la présentation du projet.
 
-Données : https://www.kaggle.com/c/home-credit-default-risk/data
+Les principaux fichiers sont :
+- eda.ipynb : Notebook Jupyter pour l'exploration des données.
+- modelisations.ipynb : Notebook Jupyter pour l'entraînement et l'évaluation des modèles.
+- data_drift.ipynb : Notebook Jupyter pour l'analyse du data drift.
+- README.md : Ce fichier README.
 
+# Instructions pour l'Exécution
+Assurez-vous d'avoir Python et les dépendances requises installées. Vous pouvez les installer en exécutant pip install -r requirements.txt.
+Exécutez les notebooks Jupyter dans l'ordre approprié pour effectuer l'exploration des données, l'entraînement des modèles et l'évaluation.
+Utilisez MLflow pour suivre les métriques d'entraînement des modèles et enregistrer les artefacts associés.
+Consultez les rapports et les résultats générés dans les répertoires correspondants pour l'analyse supplémentaire.
 
+## Ressources intéressantes
 # ML Flow
 -> https://mlflow.org/docs/latest/tracking.html
 -> https://github.com/mlflow/mlflow
@@ -19,45 +36,8 @@ Inscription Heroku Student pack :
 https://www.heroku.com/github-students
 
 - D'abord s'inscrire au programme Github (GitHub Student Developer Pack - https://education.github.com/pack)
--> très chiant car impossible de trouver un school ID d'openclassrooms -> https://education.github.com/discount_requests/14236253/additional_information
-demande faite le 01/04/24 - en attente de validation
+-> laborieux car impossible de trouver un school ID d'openclassrooms -> https://education.github.com/discount_requests/14236253/additional_information
 
-# Feature engineering
-pas besoin de pousser
-2-3 variables déjà bien
-
-# Score
-personne qui ne va pas rembourser le crédit coutera 10x plus cher (pénaliser de 10)
-cas contraire -> pénalisé de 1
-
-f-score -> se rapproche pas mal de ça mais ne répond pas totalement à mon besoin
-à mentionner mais créer une fonction de scoring vraiment custom
-
-f beta score -> à regarder
-
-make_scorer -> à regarder - à utiliser aussi pour gridsearch, etc...
-
-# Evaluation des modèles
-gridsearchCV
-comparer/créer modèles -> baseline = accorder de façon aléatoire des crédits (genre tirer à pile ou face si on accorde les crédits ou non)
-
-# Analyse de l'importance des features (globales ou locales)
-craft AI -> plateforme de MLOps
-mise en prod
-
-Modélisation : algos Catboost, LightGBM & XGBoost
-
-# se focaliser sur une seule métrique pour prendre des décisions
-# quand plusieurs métriques -> on ne sait plus laquelle suivre
-
-Setup Mac bureau : python 3.11.3 | MacBook Air puce Apple M2 OS Sonoma version 14.4.1
-
-Inspirations
-https://github.com/eleplanois/openclassRoom/tree/main/Projet_7%20Impl%C3%A9mentez%20un%20mod%C3%A8le%20de%20scoring
-https://github.com/nalron/project_credit_scoring_model
-
-
-HEROKU
 Procfile = fichier texte à la racine du dossier de l'application, pour déclarer quelle commande executer pour démarrer l'app
 https://devcenter.heroku.com/articles/getting-started-with-python#create-and-deploy-the-app
 https://devcenter.heroku.com/articles/heroku-cli-commands#heroku-apps-create-app
@@ -68,3 +48,10 @@ Deploy your code. This command pushes the main branch of the sample repo to your
 
 ATTENTION -> ne pas mettre d'extension après Procfile ("Procfile" et non "Procfile.txt") -> sinon le fichier ne sera pas reconnu par Heroku
 ATTENTION (bis) -> bien mettre Procfile à la racine du projet (et non dans le dossier de l'app)
+
+Liste des fichiers pour déploiement heroku:
+- runtime.txt -> pour spécifier version de python utilisée dans heroku
+- requirements.txt -> pour spécifier librairies à installer et leurs versions
+- Procfile -> pour spécifier quelle app lancer et comment
+
+Lien vers le dashboard intéractif : https://lit-cove-87268-9b9a2d0fbdb8.herokuapp.com/
--- scoring_app/app.py
+++ scoring_app/app.py
@@ -8,59 +8,65 @@ import os
 import shap
 import matplotlib.pyplot as plt
 
+# Configuration de la page
+st.set_page_config(
+    page_title = 'Scoring crédit',
+    page_icon = 'scoring_app/app_illustrations/pret_a_depenser_logo.png',
+    layout = 'wide',
+    menu_items = {
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+        }
+    )
 
-@st.cache(suppress_st_warning = True)
-
-def get_fvalue(val):
-    feature_dict = {'No' : 1 ,'Yes' : 2}
-    for key, value in feature_dict.items():
-        if val == key:
-            return value
-
-def get_value(val, my_dict):
-    for key, value in my_dict.items():
-        if val == key:
-            return value
-
-app_mode = st.sidebar.selectbox('Select Page', ['Home', 'Prediction']) #two pages
-
+# Définition des deux pages de l'application
+st.sidebar.image('scoring_app/app_illustrations/pret_a_depenser_logo.png')
+app_mode = st.sidebar.selectbox('Select Page', [
+    'Home', # page d'accueil et description des variables
+    'Prediction' # page pour faire les prédictions et expliquer le choix
+    ])
 
 if app_mode == 'Home':
-    st.title('LOAN PREDICTION :')
+    st.title('SCORING CREDIT')
+    st.divider()
+    st.subheader("Outil de scoring crédit pour calculer la probabilité qu’un client rembourse son crédit, et classifier la demande en crédit accordé ou refusé")
+    st.divider()
     st.image('scoring_app/app_illustrations/paying-off-a-loan-early.jpg')
-    st.markdown('Dataset :')
 
-    path = ''
-    filepath = os.path.join(path, 'data/df_merged')
-    with open(filepath, 'rb') as f:
-        data = pickle.load(f)
-
-    st.write(data.head())
-    st.markdown('Applicant Income VS Loan Amount ')
-    st.bar_chart(data[['AMT_INCOME_TOTAL', 'AMT_CREDIT']].head(20))
-
-    st.markdown("Visualiser les données d'un prêt:")
-    loan_id = txt = st.text_input("Entrez l'ID du prêt...")
-    if st.button('Voir les données'):
-        # faire la prédiction en utilisant le modèle entrainé
-        st.write(data.loc[data['SK_ID_CURR'] == loan_id])
+    # à défaut d'afficher infos sur jeu de données, décrire les features utilisées:
+    st.subheader("Liste des descripteurs utilisés pour prédire l'attribution de prêts :")
+    st.write("- :blue[CNT_CHILDREN]     :     Nombre d'enfants qu'à le/la client(e)")
+    st.write("- :blue[CNT_FAM_MEMBERS]     :     Nombre de membres dans la famille")
+    st.write("- :blue[PREVIOUS_LOANS_COUNT]     :     Nombre total des précédents crédits pris par chaque client")
+    st.write("- :blue[NONLIVINGAREA_MODE]     :     Informations normalisées sur l'immeuble où vit le client (taille, étages, etc...)")
+    st.write("- :blue[AMT_REQ_CREDIT_BUREAU_QRT]     :     Nombre de demandes de renseignements auprès du bureau de crédit concernant le client 3 mois avant la demande (à l'exclusion du mois précédant la demande)")
+    st.write("- :blue[AMT_REQ_CREDIT_BUREAU_YEAR]     :     Nombre de demandes de renseignements auprès du bureau de crédit concernant le client sur un an (à l'exclusion des 3 derniers mois avant la demande)")
+    st.write("- :blue[EXT_SOURCE_3]     :     Score normalisé provenant d'une source de données externe.")
+    st.write("- :blue[OBS_30_CNT_SOCIAL_CIRCLE]     :     Combien d'observations des environs sociaux du client avec un défaut observable de 30 jours de retard (30 DPD).")
+    st.write("- :blue[OBS_60_CNT_SOCIAL_CIRCLE]     :     Combien d'observations des environs sociaux du client avec un défaut observable de 60 jours de retard (30 DPD).")
+    st.write("- :blue[DEF_30_CNT_SOCIAL_CIRCLE]     :     Combien d'observations des environs sociaux du client ont fait défaut avec un retard de paiement de 30 jours (30 DPD)")
 
 elif app_mode == 'Prediction':
-    st.image('scoring_app/app_illustrations/multi-currency-iban.jpg')
-    st.subheader('Bonjour, merci de remplir les informations suivantes à propos du client afin de déterminer si nous devons acceder à sa demande de prêt:')
+    st.image('scoring_app/app_illustrations/multi-currency-iban.jpg', width = 800)
+    phrase = '''
+    Bonjour,
+    merci de remplir les informations suivantes à propos du client afin de déterminer si nous devons acceder à sa demande de prêt
+    '''
+    st.subheader(phrase)
+
     st.sidebar.header('Informations à propos du client:')
 
-
-    childrennumber = st.sidebar.radio("Nombre d'enfants", options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) # CNT_CHILDREN = 'Number of children the client has'
-    obs_30_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (30 derniers jours)", 0, 5, 0) # OBS_30_CNT_SOCIAL_CIRCLE = "How many observation of client's social surroundings with observable 30 DPD (days past due) default"
-    nonlivingarea_mode = st.sidebar.slider('Non living area mode (normalisation)', 0.0, 10.0, 0.1) # NONLIVINGAREA_MODE = 'Normalized information about building where the client lives, What is average (_AVG suffix), modus (_MODE suffix), median (_MEDI suffix) apartment size, common area, living area, age of building, number of elevators, number of entrances, state of the building, number of floor'
-    ext_source_3 = st.sidebar.slider('Normalized score for external datasource', 0.0, 1.0, 0.1) # EXT_SOURCE_3 = 'Normalized score from external data source'
-    def_30_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (30 jours par défaut)", 0, 5, 0) # DEF_30_CNT_SOCIAL_CIRCLE = "How many observation of client's social surroundings defaulted on 30 DPD (days past due) "
-    amt_req_credit_bureau_qrt = st.sidebar.slider('Nb enquêtes 3 derniers mois', 0, 5, 0) # AMT_REQ_CREDIT_BUREAU_QRT = 'Number of enquiries to Credit Bureau about the client 3 month before application (excluding one month before application)'
-    previous_loans_count = st.sidebar.radio('Nb de prêts antérieurs', options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) # PREVIOUS_LOANS_COUNT - nombre de prêts précedants
-    amt_req_credit_bureau_year = st.sidebar.slider('Nb enquêtes année passée', 0, 5, 0) # AMT_REQ_CREDIT_BUREAU_YEAR = 'Number of enquiries to Credit Bureau about the client one day year (excluding last 3 months before application)'
-    obs_60_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (60 jours par défaut)", 0, 5, 0) # OBS_60_CNT_SOCIAL_CIRCLE = "How many observation of client's social surroundings with observable 60 DPD (days past due) default"
-    cnt_fam_members = st.sidebar.selectbox('Nb de membres dans la famille', (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) # CNT_FAM_MEMBERS = 'How many family members does client have'
+    childrennumber = st.sidebar.radio("Nombre d'enfants", options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]) # CNT_CHILDREN = 'Number of children the client has'
+    obs_30_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (30 derniers jours)", 0, 350, 0) # OBS_30_CNT_SOCIAL_CIRCLE = "How many observation of client's social surroundings with observable 30 DPD (days past due) default"
+    nonlivingarea_mode = st.sidebar.slider('Non living area mode (normalisation)', 0.0, 1.0, 0.01) # NONLIVINGAREA_MODE = 'Normalized information about building where the client lives, What is average (_AVG suffix), modus (_MODE suffix), median (_MEDI suffix) apartment size, common area, living area, age of building, number of elevators, number of entrances, state of the building, number of floor'
+    ext_source_3 = st.sidebar.slider('Normalized score for external datasource', 0.0, 1.0, 0.01) # EXT_SOURCE_3 = 'Normalized score from external data source'
+    def_30_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (30 jours par défaut)", 0, 35, 0) # DEF_30_CNT_SOCIAL_CIRCLE = "How many observation of client's social surroundings defaulted on 30 DPD (days past due) "
+    amt_req_credit_bureau_qrt = st.sidebar.slider('Nb enquêtes 3 derniers mois', 0, 300, 0) # AMT_REQ_CREDIT_BUREAU_QRT = 'Number of enquiries to Credit Bureau about the client 3 month before application (excluding one month before application)'
+    previous_loans_count = st.sidebar.slider('Nb de prêts antérieurs', 0, 150, 0) # PREVIOUS_LOANS_COUNT - nombre de prêts précedants
+    amt_req_credit_bureau_year = st.sidebar.slider('Nb enquêtes année passée', 0, 30, 0) # AMT_REQ_CREDIT_BUREAU_YEAR = 'Number of enquiries to Credit Bureau about the client one day year (excluding last 3 months before application)'
+    obs_60_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (60 jours par défaut)", 0, 350, 0) # OBS_60_CNT_SOCIAL_CIRCLE = "How many observation of client's social surroundings with observable 60 DPD (days past due) default"
+    cnt_fam_members = st.sidebar.selectbox('Nb de membres dans la famille', (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25)) # CNT_FAM_MEMBERS = 'How many family members does client have'
 
     # Données du client:
     data1 = {
@@ -76,17 +82,18 @@ elif app_mode == 'Prediction':
          'CNT_FAM_MEMBERS' : cnt_fam_members 
          }
 
+    # Lister les features dans le même ordre que graph shap :
     feature_list = [
-        childrennumber,
-        obs_30_cnt_social_circle,
-        nonlivingarea_mode,
         ext_source_3,
-        def_30_cnt_social_circle,
+        obs_30_cnt_social_circle,
         amt_req_credit_bureau_qrt,
-        previous_loans_count,
-        amt_req_credit_bureau_year,
         obs_60_cnt_social_circle,
-        cnt_fam_members
+        nonlivingarea_mode,
+        previous_loans_count,
+        cnt_fam_members,
+        def_30_cnt_social_circle,
+        amt_req_credit_bureau_year,
+        childrennumber
         ]
 
     single_sample = np.array(feature_list).reshape(1, -1)
@@ -114,6 +121,7 @@ elif app_mode == 'Prediction':
             st.success('Selon notre prédiction, le prêt sera accordé')
             st.markdown(f'<img src="data:image/gif;base64, {data_url}" alt="cat gif">', unsafe_allow_html = True)
 
+        st.divider()
 
         # Afficher l'explication de la prédiction (waterfall plot)
         explainer = pickle.load(open('explainer', 'rb'))

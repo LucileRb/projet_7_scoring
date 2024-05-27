bbc25693c77f00bfaf27942c95d71e8bd749980e
LucileRb <124692104+Lucile-ads-up@users.noreply.github.com>
2024-05-27 17:46:07 +0200

    test avec une autre url

--- scoring_app/app.py
+++ scoring_app/app.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import requests
 
 def get_prediction(data):
-    api_url = 'https://scoring-credit-implementation-a56784ea5721.herokuapp.com/Prediction' # à remplacer par bonne url de l'api streamlit
+    api_url = 'http://18.201.108.103:8501/Prediction' # à remplacer par bonne url de l'api streamlit
     print(data)
     response = requests.post(api_url, json = data)
     print('réponse api')

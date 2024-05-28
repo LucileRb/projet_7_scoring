ba7d01ae9aed8be2a678e89018e9c4f7c31dd5cd
LucileRb <124692104+Lucile-ads-up@users.noreply.github.com>
2024-05-28 09:42:34 +0200

    test en ajoutant api dans procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: sh scoring_app/setup.sh && streamlit run scoring_app/app.py
+api: python scoring_app/api.py
--- scoring_app/api.py
+++ scoring_app/api.py
@@ -49,4 +49,4 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host = '127.0.0.1', port = 5000)

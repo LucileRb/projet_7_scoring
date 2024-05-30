3b1cbf222fb0df3c82ddebb7d6fbe64ffe9d6bc2
LucileRb <124692104+Lucile-ads-up@users.noreply.github.com>
2024-05-30 13:39:29 +0200

    test sans x test parquet

--- scoring_app/app.py
+++ scoring_app/app.py
@@ -1,4 +1,4 @@
-#### IMPORTS #####
+################################################################################################ IMPORTS ################################################################################################
 import base64
 import streamlit as st
 import pandas as pd
@@ -13,7 +13,7 @@ import requests
 import pyarrow.parquet as pq
 import json
 
-################################################ FONCTIONS ################################################
+################################################################################################ FONCTIONS ################################################################################################
 
 def get_prediction(data):
     api_url = 'https://scoring-credit-implementation-a56784ea5721.herokuapp.com/Prediction' # url de l'api sur heroku
@@ -121,12 +121,12 @@ def visualize_shap_values(selected_client_data, prediction_score):
     st.pyplot(fig)
 
 
-################################################ DATA & OUTILS ################################################
+################################################################################################ DATA & OUTILS ################################################################################################
 
 
 # Load sample parquet data
-parquet_file = 'utils/X_test_SS.parquet'
-df = pq.read_table(parquet_file).to_pandas().reset_index(drop = True)
+#parquet_file = 'utils/X_test_SS.parquet'
+#df = pq.read_table(parquet_file).to_pandas().reset_index(drop = True)
 
 parquet_file_xtrain = 'utils/X_train_SS.parquet'
 df_x_train = pq.read_table(parquet_file_xtrain).to_pandas().reset_index(drop = True)
@@ -158,7 +158,7 @@ features = [
     ]
 
 
-################################################ DASHBOARD ################################################
+################################################################################################ DASHBOARD ################################################################################################
 
 
 # Configuration de la page
@@ -192,9 +192,9 @@ if app_mode == 'Home':
     st.write("- :blue[AMT_REQ_CREDIT_BUREAU_QRT]     :     Nombre de demandes de renseignements auprès du bureau de crédit concernant le client 3 mois avant la demande (à l'exclusion du mois précédant la demande)")
     st.write("- :blue[AMT_REQ_CREDIT_BUREAU_YEAR]     :     Nombre de demandes de renseignements auprès du bureau de crédit concernant le client sur un an (à l'exclusion des 3 derniers mois avant la demande)")
     st.write("- :blue[EXT_SOURCE_3]     :     Score normalisé provenant d'une source de données externe.")
-    st.write("- :blue[OBS_30_CNT_SOCIAL_CIRCLE]     :     Combien d'observations des environs sociaux du client avec un défaut observable de 30 jours de retard (30 DPD).")
-    st.write("- :blue[OBS_60_CNT_SOCIAL_CIRCLE]     :     Combien d'observations des environs sociaux du client avec un défaut observable de 60 jours de retard (30 DPD).")
-    st.write("- :blue[DEF_30_CNT_SOCIAL_CIRCLE]     :     Combien d'observations des environs sociaux du client ont fait défaut avec un retard de paiement de 30 jours (30 DPD)")
+    st.write("- :blue[OBS_30_CNT_SOCIAL_CIRCLE]     :     Nombre d'observations des environs sociaux du client avec un défaut observable de 30 jours de retard (30 DPD).")
+    st.write("- :blue[OBS_60_CNT_SOCIAL_CIRCLE]     :     Nombre d'observations des environs sociaux du client avec un défaut observable de 60 jours de retard (30 DPD).")
+    st.write("- :blue[DEF_30_CNT_SOCIAL_CIRCLE]     :     Nombre d'observations des environs sociaux du client ont fait défaut avec un retard de paiement de 30 jours (30 DPD)")
 
 
 elif app_mode == 'Client ID':
@@ -259,16 +259,16 @@ elif app_mode == 'New prediction':
 
     st.sidebar.header('Informations à propos du client:')
 
-    childrennumber = st.sidebar.radio("Nombre d'enfants", options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]) # CNT_CHILDREN = 'Number of children the client has'
-    obs_30_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (30 derniers jours)", 0, 350, 0) # OBS_30_CNT_SOCIAL_CIRCLE = "How many observation of client's social surroundings with observable 30 DPD (days past due) default"
-    nonlivingarea_mode = st.sidebar.slider('Non living area mode (normalisation)', 0.0, 1.0, 0.01) # NONLIVINGAREA_MODE = 'Normalized information about building where the client lives, What is average (_AVG suffix), modus (_MODE suffix), median (_MEDI suffix) apartment size, common area, living area, age of building, number of elevators, number of entrances, state of the building, number of floor'
-    ext_source_3 = st.sidebar.slider('Normalized score for external datasource', 0.0, 1.0, 0.01) # EXT_SOURCE_3 = 'Normalized score from external data source'
-    def_30_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (30 jours par défaut)", 0, 35, 0) # DEF_30_CNT_SOCIAL_CIRCLE = "How many observation of client's social surroundings defaulted on 30 DPD (days past due) "
-    amt_req_credit_bureau_qrt = st.sidebar.slider('Nb enquêtes 3 derniers mois', 0, 300, 0) # AMT_REQ_CREDIT_BUREAU_QRT = 'Number of enquiries to Credit Bureau about the client 3 month before application (excluding one month before application)'
-    previous_loans_count = st.sidebar.slider('Nb de prêts antérieurs', 0, 150, 0) # PREVIOUS_LOANS_COUNT - nombre de prêts précedants
-    amt_req_credit_bureau_year = st.sidebar.slider('Nb enquêtes année passée', 0, 30, 0) # AMT_REQ_CREDIT_BUREAU_YEAR = 'Number of enquiries to Credit Bureau about the client one day year (excluding last 3 months before application)'
-    obs_60_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (60 jours par défaut)", 0, 350, 0) # OBS_60_CNT_SOCIAL_CIRCLE = "How many observation of client's social surroundings with observable 60 DPD (days past due) default"
-    cnt_fam_members = st.sidebar.selectbox('Nb de membres dans la famille', (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25)) # CNT_FAM_MEMBERS = 'How many family members does client have'
+    childrennumber = st.sidebar.radio("Nombre d'enfants", options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+    obs_30_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (30 derniers jours)", 0, 350, 0)
+    nonlivingarea_mode = st.sidebar.slider('Non living area mode (normalisation)', 0.0, 1.0, 0.01)
+    ext_source_3 = st.sidebar.slider('Normalized score for external datasource', 0.0, 1.0, 0.01)
+    def_30_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (30 jours par défaut)", 0, 35, 0)
+    amt_req_credit_bureau_qrt = st.sidebar.slider('Nb enquêtes 3 derniers mois', 0, 300, 0)
+    previous_loans_count = st.sidebar.slider('Nb de prêts antérieurs', 0, 150, 0)
+    amt_req_credit_bureau_year = st.sidebar.slider('Nb enquêtes année passée', 0, 30, 0)
+    obs_60_cnt_social_circle = st.sidebar.slider("Nb d'observations de l'entourage du client (60 jours par défaut)", 0, 350, 0)
+    cnt_fam_members = st.sidebar.selectbox('Nb de membres dans la famille', (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25))
 
     # Données du client:
     data1 = {
@@ -352,3 +352,6 @@ elif app_mode == 'New prediction':
             st.subheader('Feature Contribution:')
             visualize_shap_values(single_sample, prediction_score)
             st.text("Bar chart and force plot showing the features that contribute the most to the credit score globally and for the selected client.")
+
+
+################################################################################################ END ################################################################################################
